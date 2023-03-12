aa0036811526e01303ec600623698f5204149802
wangyunke <wangyunke@didiglobal.com>
2023-03-12 16:06:58 +0800

    [Feature]不需要activity创建fragment
    Signed-off-by: wangyunke <wangyunke@didiglobal.com>

---
+++ app/src/main/java/com/i/MyApplication.java
@@ -0,0 +1,40 @@
+package com.i;
+
+import android.app.Application;
+import android.content.Context;
+import android.os.Handler;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentController;
+import androidx.fragment.app.FragmentHostCallback;
+import androidx.fragment.app.FragmentTransaction;
+
+public class MyApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+    void createFragment(Fragment fragment, int viewId){
+        HostCallBack callBack = new HostCallBack(this, new Handler(), 0);
+        FragmentController controller = FragmentController.createController(callBack);
+        FragmentTransaction transaction = controller.getSupportFragmentManager().beginTransaction();
+        transaction.add(viewId, fragment);
+        transaction.commit();
+    }
+
+    static class HostCallBack extends FragmentHostCallback {
+        public HostCallBack(@NonNull Context context, @NonNull Handler handler, int windowAnimations) {
+            super(context, handler, windowAnimations);
+        }
+
+        @Nullable
+        @Override
+        public Object onGetHost() {
+            return null;
+        }
+    }
+}
