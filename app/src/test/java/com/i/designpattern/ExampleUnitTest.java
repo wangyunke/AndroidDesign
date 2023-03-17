8ba012935d5e81b51e9ec632c06c09b1a1f7fb4e
wangyunke <wangyunke@didiglobal.com>
2023-03-17 16:39:32 +0800

    [Feature]十进制：两个数异或相同为0，0和所有数异或等于数本身
    Signed-off-by: wangyunke <wangyunke@didiglobal.com>

--- app/src/test/java/com/i/designpattern/ExampleUnitTest.java
+++ app/src/test/java/com/i/designpattern/ExampleUnitTest.java
@@ -29,4 +29,19 @@ public class ExampleUnitTest {
         owner.makeChange();
     }
 
+    /**
+     * 二进制：相同为0，不同为1
+     * 十进制：相同为0，不同再计算，0^数=数
+     */
+    @Test
+    public void numOr() {
+        int value = 5^5;
+        int value1 = 0^15;
+        int value2 = 6^5;
+        System.out.println("5^5="+value);
+        System.out.println("0^15="+value1);
+        System.out.println("6^5="+value2);
+        System.out.println("6^5^7^19^5^7^6^19^199="+(6^5^7^19^5^7^6^19^199));
+    }
+
 }
