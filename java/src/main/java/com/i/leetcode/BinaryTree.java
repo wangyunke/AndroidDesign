c0a4968a36d9bcd8bf42f69947ae9b0e0bbf2058
wangyunke <extern.wang.yunke@mobilityasia.com.cn>
2023-10-20 10:37:56 +0800

    [Feature]leetcode: BFS
    Signed-off-by: wangyunke <extern.wang.yunke@mobilityasia.com.cn>

--- java/src/main/java/com/i/leetcode/BinaryTree.java
+++ java/src/main/java/com/i/leetcode/BinaryTree.java
@@ -1,26 +1,19 @@
 package com.i.leetcode;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 import java.util.Stack;
 
 public class BinaryTree {
-    public class TreeNode {
-        int val = 0;
-        TreeNode left = null;
-        TreeNode right = null;
 
-        public TreeNode(int val) {
-            this.val = val;
-        }
-    }
-
-    public static List<Integer> dfs(TreeNode root) {
+    public static List<Integer> DFS(TreeNode root) {
         if (root == null) return null;
         List<Integer> result = new ArrayList<>();
         Stack<TreeNode> stack = new Stack<>();
-        stack.push(root);
 
+        stack.push(root);
         while (!stack.empty()) {
             TreeNode node = stack.pop();
             result.add(node.val);
@@ -35,5 +28,32 @@ public class BinaryTree {
         return result;
     }
 
+    public static List<Integer> BFS(TreeNode root) {
+        if (root == null) return null;
+        List<Integer> result = new ArrayList<>();
+        Queue<TreeNode> queue = new LinkedList<>();
 
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            TreeNode node = queue.poll();
+            result.add(node.val);
+
+            if (node.left != null) {
+                queue.add(node.left);
+            }
+            if (node.right != null) {
+                queue.add(node.right);
+            }
+        }
+        return result;
+    }
+
+    public class TreeNode {
+        int val = 0;
+        TreeNode left = null;
+        TreeNode right = null;
+        public TreeNode(int val) {
+            this.val = val;
+        }
+    }
 }
