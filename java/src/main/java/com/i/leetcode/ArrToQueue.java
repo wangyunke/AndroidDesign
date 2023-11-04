ef2e48200b5c6db1d8c5b301ba28611282782c88
wangyunke <wangyunke@didiglobal.com>
2023-11-04 17:18:04 +0800

    [Feature]code ArrToQueue: isEmpty isFull enQueue deQueue
    Signed-off-by: wangyunke <wangyunke@didiglobal.com>

---
+++ java/src/main/java/com/i/leetcode/ArrToQueue.java
@@ -0,0 +1,54 @@
+package com.i.leetcode;
+
+import jdk.internal.org.jline.utils.Log;
+
+public class ArrToQueue {
+
+    static class Queue {
+        int[] arr;
+        int maxSize;
+        int lastIndex = 0;
+        int frontIndex = 0;
+
+        Queue(int maxSize) {
+            arr = new int[maxSize];
+        }
+
+        void enQueue(int val) {
+            if (isFull()) {
+                Log.error("queue full");
+            } else {
+                arr[lastIndex++] = val;
+            }
+        }
+
+        int deQueue() {
+            if (isEmpty()) {
+                Log.error("queue empty");
+                return -1;
+            } else {
+                return arr[frontIndex++];
+            }
+        }
+
+        void show() {
+            for (int i = frontIndex; i <= lastIndex; i++) {
+                Log.info(arr[frontIndex]);
+            }
+        }
+
+        int size() {
+            int diff = lastIndex - frontIndex;
+            return Math.max(diff, 0);
+        }
+
+        boolean isEmpty() {
+            return frontIndex > lastIndex;
+        }
+
+        boolean isFull() {
+            return lastIndex > maxSize - 1;
+        }
+    }
+
+}
--- java/src/main/java/com/i/print/Transfer3.java
+++ java/src/main/java/com/i/print/Transfer3.java
@@ -5,7 +5,10 @@ public class Transfer3 {
     public static void main(String[] args) {
 
         int[] nums={1,7,8,0,1};
-
+        int index=0;
+        System.out.println("----"+nums[index]);
+        System.out.println("----"+nums[index++]);
+        System.out.println("----"+index);
 
     }
 
