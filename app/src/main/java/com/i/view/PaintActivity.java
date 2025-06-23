530fccc205db49af742f03cf17457303bad027fd
Yunke Wang <extern.yunke.wang@cariad-technology.cn>
2025-06-23 14:25:40 +0800

    [Feature]rich text
    Signed-off-by: Yunke Wang <extern.yunke.wang@cariad-technology.cn>

--- app/src/main/java/com/i/view/PaintActivity.java
+++ app/src/main/java/com/i/view/PaintActivity.java
@@ -1,35 +1,33 @@
 package com.i.view;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.LayoutInflater;
+import android.text.Html;
+import android.text.Spanned;
 import android.view.View;
 
 import com.i.designpattern.R;
 import com.i.designpattern.activity.BaseActivity;
-import com.i.hook.Catcher;
-import com.i.service.WindowViewService;
+import com.i.designpattern.databinding.ActivityPaintBinding;
 
 public class PaintActivity extends BaseActivity {
+    private ActivityPaintBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_paint);
-
-
+        binding = ActivityPaintBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
         onClick();
     }
 
     private void onClick() {
         TrackView trackView = findViewById(R.id.tv);
-        findViewById(R.id.clean).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                trackView.clean();
-            }
-        });
+        binding.clean.setOnClickListener(v -> trackView.clean());
+
+        Spanned richText = Html.
+                fromHtml("1、水电费加快速度见附件<br/>2、圣诞节疯狂拒收到付<br/>3、收到金陵饭店",
+                Html.FROM_HTML_MODE_LEGACY);
+        binding.rich.setText(richText);
     }
 }
